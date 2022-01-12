484b26d80dc78dae18d5deeb690e42af09b94179
Roger Lefort <tirogerl@gmail.com>
2022-01-12 01:13:33 -0800

    Added stats section and map

--- app.py
+++ app.py
@@ -7,9 +7,16 @@ import pandas as pd
 import numpy as np
 
 import folium
+from folium.features import CustomIcon
+from folium.plugins import HeatMap
+from streamlit_folium import folium_static
 import plotly
+import datetime
+
+from PIL import Image
 
 import streamlit as st
+
 
 #############
 # STREAMLIT #
@@ -31,10 +38,12 @@ st.set_page_config(
 #################
 # FETCHING DATA #
 # ###############
-@st.cache
+
+@st.cache(show_spinner=False)
 def fetch_and_clean_data():
-    collisions_df = fetch_data()
-    return collisions_df
+    with st.spinner('Data Refreshing... May take up to 5 minutes.'):
+        collisions_df = fetch_data()
+        return collisions_df
 
 data = fetch_and_clean_data()
 
@@ -43,63 +52,70 @@ data = fetch_and_clean_data()
 # TRANSFORMING DATA FOR METRICS #
 ##################################
 
-collisions = data.copy()
-last_updated = collisions['CRASH DATE'].max()
+data_start_date = '2018-12-31' # Limiting data to from 2020 to present day
 
-grouped_by_month = collisions.groupby(['CRASH YEAR', 'CRASH MONTH-YEAR']).agg({
-    'COLLISION_ID': 'count',
-    'NUMBER OF PERSONS INJURED': 'sum',
-    'NUMBER OF PERSONS KILLED': 'sum',
-    'NUMBER OF PEDESTRIANS INJURED': 'sum',
-    'NUMBER OF PEDESTRIANS KILLED': 'sum',
-    'NUMBER OF CYCLIST INJURED': 'sum',
-    'NUMBER OF CYCLIST KILLED': 'sum',
-    'NUMBER OF MOTORIST INJURED': 'sum',
-    'NUMBER OF MOTORIST KILLED': 'sum'
+
+collisions = data.copy()
+
+collisions = collisions[collisions['crash_date'] > data_start_date]
+
+last_updated = collisions['crash_date'].max()
+first_date = collisions['crash_date'].min()
+
+grouped_by_day = collisions.groupby(['crash_year', 'crash_date']).agg({
+    'collision_id': 'count',
+    'number_of_persons_injured': 'sum',
+    'number_of_persons_killed': 'sum',
+    'number_of_pedestrians_injured': 'sum',
+    'number_of_pedestrians_killed': 'sum',
+    'number_of_cyclist_injured': 'sum',
+    'number_of_cyclist_killed': 'sum',
+    'number_of_motorist_injured': 'sum',
+    'number_of_motorist_killed': 'sum'
 })
-grouped_by_month.reset_index(inplace=True)
+grouped_by_day.reset_index(inplace=True)
 
 # Grouping by Year to obtain the cummulative sum
-grouped_by_month['collisions_cumsum'] = grouped_by_month.groupby(['CRASH YEAR'])['COLLISION_ID'].cumsum()
+grouped_by_day['collisions_cumsum'] = grouped_by_day.groupby(['crash_year'])['collision_id'].cumsum()
 
-grouped_by_month['person_injured_cumsum'] = grouped_by_month.groupby(['CRASH YEAR'])['NUMBER OF PERSONS INJURED'].cumsum()
-grouped_by_month['person_killed_cumsum'] = grouped_by_month.groupby(['CRASH YEAR'])['NUMBER OF PERSONS KILLED'].cumsum()
+grouped_by_day['person_injured_cumsum'] = grouped_by_day.groupby(['crash_year'])['number_of_persons_injured'].cumsum()
+grouped_by_day['person_killed_cumsum'] = grouped_by_day.groupby(['crash_year'])['number_of_persons_killed'].cumsum()
 
-grouped_by_month['peds_injured_cumsum'] = grouped_by_month.groupby(['CRASH YEAR'])['NUMBER OF PEDESTRIANS INJURED'].cumsum()
-grouped_by_month['peds_killed_cumsum'] = grouped_by_month.groupby(['CRASH YEAR'])['NUMBER OF PEDESTRIANS KILLED'].cumsum()
+grouped_by_day['peds_injured_cumsum'] = grouped_by_day.groupby(['crash_year'])['number_of_pedestrians_injured'].cumsum()
+grouped_by_day['peds_killed_cumsum'] = grouped_by_day.groupby(['crash_year'])['number_of_pedestrians_killed'].cumsum()
 
-grouped_by_month['cyclist_injured_cumsum'] = grouped_by_month.groupby(['CRASH YEAR'])['NUMBER OF CYCLIST INJURED'].cumsum()
-grouped_by_month['cyclist_killed_cumsum'] = grouped_by_month.groupby(['CRASH YEAR'])['NUMBER OF CYCLIST KILLED'].cumsum()
-grouped_by_month.reset_index(inplace=True)
+grouped_by_day['cyclist_injured_cumsum'] = grouped_by_day.groupby(['crash_year'])['number_of_cyclist_injured'].cumsum()
+grouped_by_day['cyclist_killed_cumsum'] = grouped_by_day.groupby(['crash_year'])['number_of_cyclist_killed'].cumsum()
+grouped_by_day.reset_index(inplace=True)
 
 # Year to Date metrics
-max_date = collisions['CRASH MONTH-YEAR'].max()
+max_date = collisions['crash_date'].max()
 
-collisions_YTD = grouped_by_month.loc[grouped_by_month['CRASH MONTH-YEAR'] == max_date]['collisions_cumsum']
+collisions_YTD = grouped_by_day.loc[grouped_by_day['crash_date'] == max_date]['collisions_cumsum']
 
-# persons_injured_YTD = grouped_by_month.loc[grouped_by_month['CRASH MONTH-YEAR'] == max_date]['person_injured_cumsum']
-# persons_killed_YTD = grouped_by_month.loc[grouped_by_month['CRASH MONTH-YEAR'] == max_date]['person_killed_cumsum']
+# persons_injured_YTD = grouped_by_month.loc[grouped_by_month['crash_month_year'] == max_date]['person_injured_cumsum']
+# persons_killed_YTD = grouped_by_month.loc[grouped_by_month['crash_month_year'] == max_date]['person_killed_cumsum']
 
-peds_injured_YTD = grouped_by_month.loc[grouped_by_month['CRASH MONTH-YEAR'] == max_date]['peds_injured_cumsum']
-peds_killed_YTD = grouped_by_month.loc[grouped_by_month['CRASH MONTH-YEAR'] == max_date]['peds_killed_cumsum']
+peds_injured_YTD = grouped_by_day.loc[grouped_by_day['crash_date'] == max_date]['peds_injured_cumsum']
+peds_killed_YTD = grouped_by_day.loc[grouped_by_day['crash_date'] == max_date]['peds_killed_cumsum']
 
-cyclists_injured_YTD = grouped_by_month.loc[grouped_by_month['CRASH MONTH-YEAR'] == max_date]['cyclist_injured_cumsum']
-cyclists_killed_YTD = grouped_by_month.loc[grouped_by_month['CRASH MONTH-YEAR'] == max_date]['cyclist_killed_cumsum']
+cyclists_injured_YTD = grouped_by_day.loc[grouped_by_day['crash_date'] == max_date]['cyclist_injured_cumsum']
+cyclists_killed_YTD = grouped_by_day.loc[grouped_by_day['crash_date'] == max_date]['cyclist_killed_cumsum']
 
 # Previous YTD
-last_year = max_date.to_timestamp() - pd.DateOffset(years=1)
-last_YTD= last_year.to_period('M')
+last_year = max_date - pd.DateOffset(years=1)
+last_YTD = last_year
 
-collisions_YTD_previous = grouped_by_month.loc[grouped_by_month['CRASH MONTH-YEAR'] == last_YTD]['collisions_cumsum']
+collisions_YTD_previous = grouped_by_day.loc[grouped_by_day['crash_date'] == last_YTD]['collisions_cumsum']
 
-# persons_injured_YTD_previous = grouped_by_month.loc[grouped_by_month['CRASH MONTH-YEAR'] == last_YTD]['person_injured_cumsum']
-# persons_killed_YTD_previous = grouped_by_month.loc[grouped_by_month['CRASH MONTH-YEAR'] == last_YTD]['person_killed_cumsum']
+# persons_injured_YTD_previous = grouped_by_month.loc[grouped_by_month['crash_month_year'] == last_YTD]['person_injured_cumsum']
+# persons_killed_YTD_previous = grouped_by_month.loc[grouped_by_month['crash_month_year'] == last_YTD]['person_killed_cumsum']
 
-peds_injured_YTD_previous = grouped_by_month.loc[grouped_by_month['CRASH MONTH-YEAR'] == last_YTD]['peds_injured_cumsum']
-peds_killed_YTD_previous = grouped_by_month.loc[grouped_by_month['CRASH MONTH-YEAR'] == last_YTD]['peds_killed_cumsum']
+peds_injured_YTD_previous = grouped_by_day.loc[grouped_by_day['crash_date'] == last_YTD]['peds_injured_cumsum']
+peds_killed_YTD_previous = grouped_by_day.loc[grouped_by_day['crash_date'] == last_YTD]['peds_killed_cumsum']
 
-cyclists_injured_YTD_previous = grouped_by_month.loc[grouped_by_month['CRASH MONTH-YEAR'] == last_YTD]['cyclist_injured_cumsum']
-cyclists_killed_YTD_previous = grouped_by_month.loc[grouped_by_month['CRASH MONTH-YEAR'] == last_YTD]['cyclist_killed_cumsum']
+cyclists_injured_YTD_previous = grouped_by_day.loc[grouped_by_day['crash_date'] == last_YTD]['cyclist_injured_cumsum']
+cyclists_killed_YTD_previous = grouped_by_day.loc[grouped_by_day['crash_date'] == last_YTD]['cyclist_killed_cumsum']
 
 # Percent Change YTD
 collisions_perc_change = (collisions_YTD.values - collisions_YTD_previous.values)/collisions_YTD_previous.values*100
@@ -110,25 +126,104 @@ peds_killed_perc_change = (peds_killed_YTD.values - peds_killed_YTD_previous.val
 cyclists_injured_perc_change = (cyclists_injured_YTD.values - cyclists_injured_YTD_previous.values)/cyclists_injured_YTD_previous.values*100
 cyclists_killed_perc_change = (cyclists_killed_YTD.values - cyclists_killed_YTD_previous.values)/cyclists_killed_YTD_previous.values*100
 
+####################
+# DATA FOR MAPPING #
+####################
+
+##########################
+# COLLISIONS IN PAST DAY #
+##########################
+
+# latest_collision_date = data.crash_date.max()
+
+
+
+def map_collisions(latest_collision_date):
+    latest_collision_date_df = data[data['crash_date'] == latest_collision_date].dropna()
+    locations = zip(latest_collision_date_df.latitude, latest_collision_date_df.longitude)
+    # Initializing a new map centered around NYC
+    collision_map = folium.Map(location=[40.7128, -74.0060], zoom_start=10, dragging=True, scrollWheelZoom=True, tiles='cartodbpositron')
+
+    # Adding maps layers
+    tooltip = "Click for more info!"
+    for (_, row) in latest_collision_date_df.iterrows(): # Iterrating through all teams
+        
+        icon = './Assets/traffic-accident.png'
+        icon_image = Image.open(icon)
+        
+        icon = CustomIcon(
+        np.array(icon_image),
+        icon_size=(20, 20),
+        popup_anchor=(0, -30),
+        )
+        html = '<i>Cause of Collision: </i>' + '<b>' + row.loc['contributing_factor_vehicle_1'] + '</b>' + '<br>' + '</b>' '<i>Number of Persons Injured: </i>' + '<b>' + str(row.loc['number_of_persons_injured']) + '</b>' + '<br>' + '</b>' '<i>Number of Persons Killed: </i>' + '<b>' + str(row.loc['number_of_persons_killed']) + '</b>'
+        iframe = folium.IFrame(html=html, width=330, height=100)
+        popup = folium.Popup(iframe, max_width=330, min_height=100)
+        
+        folium.Marker(location=[row.loc['latitude'], row.loc['longitude']], icon=icon, popup=popup, tooltip=tooltip).add_to(collision_map)
+        
+    HeatMap(locations).add_to(collision_map)
+    locations = zip(latest_collision_date_df.latitude, latest_collision_date_df.longitude)
+
+    folium_static(collision_map, width=1000)
+
+
+
 
 #############
 # FRONT END #
 #############
 
 st.title('How Safe are NYC Streets?')
-st.image('./Assets/Bike_Path-scaled.jpeg', caption=None, width='auto', use_column_width=True)
-st.caption(f'Data through {last_updated.strftime("%m/%d/%Y")}')
+st.image('./Assets/OPS-0333-Crossride-Illustration_FINAL.jpeg', caption=None, width='auto', use_column_width=True)
+st.caption(f'Data between {first_date.strftime("%m/%d/%Y")} and {last_updated.strftime("%m/%d/%Y")}')
 
 ### TOP ROW
+st.markdown(f'### NYC Vehicle Collision Statistics Year-to-Date (YTD) through {last_updated.strftime("%Y-%m-%d")}')
 col1, col2, col3, col4, col5 = st.columns(5)
 
-col1.metric('Vehicle Collisions (YTD)', collisions_YTD, f'{round(collisions_perc_change[0], 0)}%')
-col2.metric("Pedestrians Injured (YTD)", peds_injured_YTD, f'{round(peds_injured_perc_change[0], 0)}%')
-col3.metric("Pedestrians Killed (YTD)", peds_killed_YTD, f'{round(peds_killed_perc_change[0], 0)}%')
-col4.metric("Cyclists Injured (YTD)", cyclists_injured_YTD, f'{round(cyclists_injured_perc_change[0], 0)}%')
-col5.metric("Cyclists Killed (YTD)", cyclists_killed_YTD, f'{round(cyclists_killed_perc_change[0], 0)}%')
+# col1.metric('Vehicle Collisions (YTD)', collisions_YTD, f'{collisions_perc_change}%')
+# col2.metric("Pedestrians Injured (YTD)", peds_injured_YTD, f'{peds_injured_perc_change}%')
+# col3.metric("Pedestrians Killed (YTD)", peds_killed_YTD, f'{peds_killed_perc_change}%')
+# col4.metric("Cyclists Injured (YTD)", cyclists_injured_YTD, f'{cyclists_injured_perc_change}%')
+# col5.metric("Cyclists Killed (YTD)", cyclists_killed_YTD, f'{cyclists_killed_perc_change}%')
 
-# st.dataframe(data)
+col1.metric('Vehicle Collisions (YTD)', collisions_YTD, f'{round(collisions_perc_change[0], 0)}% (2021 YTD)')
+col2.metric("Pedestrians Injured (YTD)", peds_injured_YTD, f'{round(peds_injured_perc_change[0], 0)}% (2021 YTD)')
+col3.metric("Pedestrians Killed (YTD)", peds_killed_YTD, f'{round(peds_killed_perc_change[0], 0)}% (2021 YTD)')
+col4.metric("Cyclists Injured (YTD)", cyclists_injured_YTD, f'{round(cyclists_injured_perc_change[0], 0)}% (2021 YTD)')
+col5.metric("Cyclists Killed (YTD)", cyclists_killed_YTD, f'{round(cyclists_killed_perc_change[0], 0)}% (2021 YTD)')
+
+st.subheader('')
+
+#########################
+# SLIDER TO SELECT DATE #
+#########################
+
+all_dates = []
+
+
+
+for d in grouped_by_day.crash_date:
+    all_dates.append(d.strftime('%Y-%m-%d'))
+
+col6, col7 = st.columns(2)
+
+with col6:
+    st.markdown('### Daily Vehicle Collisions')
+    st.markdown(f'*Use slider to visualize collisions dating back to {all_dates[0]}*')
+with col7:
+    latest_collision_date = st.select_slider('',options=all_dates, value=all_dates[-1])
+    total_collisions = data[data['crash_date'] == latest_collision_date]['collision_id'].count()
+
+with st.container():
+    # st.subheader(f'Daily Vehicle Collisions')
+    # latest_collision_date = st.select_slider('',options=all_dates, value=all_dates[-1])
+    # st.info(f'**Use slider to visualize collisions dating back to {all_dates[0]}')
+    st.markdown(f'##### {total_collisions} Collisions on {latest_collision_date}**')
+    map_collisions(latest_collision_date)
+    
+
 
 st.sidebar.subheader("About")
 st.sidebar.write()
--- get_data.py
+++ get_data.py
@@ -3,32 +3,91 @@ import requests
 import numpy as np
 import pyarrow.parquet as pq
 from pyarrow import csv, parquet
+from pyarrow.csv import read_csv, ParseOptions, ConvertOptions, ReadOptions
 import datetime
 import pandas as pd
+from sodapy import Socrata
+import config
+APP_TOKEN = os.environ.get("SOCRATA_APP_TOKEN")
 
+
+# def fetch_data():
+#     data_path = r'./Data/'
+#     if not os.path.exists(data_path):
+#         os.makedirs(data_path)
+
+#     # # Fetching new data
+#     # URL = f'https://data.cityofnewyork.us/resource/h9gi-nx95.csv?$$app_token={APP_TOKEN}&$limit=100000'
+#     URL = 'https://data.cityofnewyork.us/api/views/h9gi-nx95/rows.csv?accessType=DOWNLOAD' # URL to fetch data in csv format
+#     r = requests.get(URL, allow_redirects=True)
+#     open(os.path.join(data_path, 'collisions.csv'), 'wb').write(r.content) # Downloading csv
+
+#     parse_options = ParseOptions(ignore_empty_lines=True)
+#     collisions_csv = csv.read_csv(os.path.join(data_path, 'collisions.csv'), parse_options=parse_options) # Opening downloaded csv with pyarrow
+#     parquet.write_table(collisions_csv, os.path.join(data_path, 'collisions.parquet')) # Saving csv as parquet file
+#     collisions_pq = pq.read_table(os.path.join(data_path, 'collisions.parquet'))
+#     if (os.path.join(data_path, 'collisions.parquet')):
+#         print('Data successfully downloaded and processed!')
+#     else:
+#         print('Error in processing data!')
+#     os.remove(os.path.join(data_path, 'collisions.csv'))
+#     collisions_df = collisions_pq.to_pandas()
+    
+#     # Cleaning dataset: replacing values identified as erroneous; update as more errors are identified
+#     collisions_df.columns = collisions_df.columns.str.replace(' ','_').str.lower()
+#     collisions_df.drop(['location', 'on_street_name', 'cross_street_name', 'off_street_name', 'vehicle_type_code_1', 'vehicle_type_code_2', 'vehicle_type_code_3', 'vehicle_type_code_4', 'vehicle_type_code_5', 'contributing_factor_vehicle_2', 'contributing_factor_vehicle_3', 'contributing_factor_vehicle_4', 'contributing_factor_vehicle_5'], axis=1, inplace=True)
+#     # collisions_df.dropna(inplace=True) # Dropping all rows with missing values; may revist this later to augment data
+
+#     # Creating new columns for analysis
+#     collisions_df['crash_date'] = pd.to_datetime(collisions_df['crash_date'])
+#     collisions_df['crash_year'] = pd.DatetimeIndex(collisions_df['crash_date']).year
+#     collisions_df['crash_month_year'] = pd.to_datetime(collisions_df['crash_date']).dt.to_period('M')
+#     collisions_df['crash_time'] = pd.to_datetime(collisions_df['crash_time'])
+#     collisions_df['crash_hour'] = pd.DatetimeIndex(collisions_df['crash_time']).hour
+
+#     collisions_df.number_of_persons_injured.fillna(0, inplace=True)
+#     collisions_df.number_of_persons_killed.fillna(0, inplace=True)
+#     collisions_df.number_of_pedestrians_injured.fillna(0, inplace=True)
+#     collisions_df.number_of_pedestrians_killed.fillna(0, inplace=True)
+#     collisions_df.number_of_cyclist_injured.fillna(0, inplace=True)
+#     collisions_df.number_of_cyclist_killed.fillna(0, inplace=True)
+#     collisions_df.number_of_motorist_injured.fillna(0, inplace=True)
+#     collisions_df.number_of_motorist_killed.fillna(0, inplace=True)
+
+#     # Converting objects to int
+#     cols_to_convert = ['number_of_persons_injured', 'number_of_persons_killed',
+#         'number_of_pedestrians_injured', 'number_of_pedestrians_killed',
+#         'number_of_cyclist_injured', 'number_of_cyclist_killed',
+#         'number_of_motorist_injured', 'number_of_motorist_killed']
+#     collisions_df[cols_to_convert] = collisions_df[cols_to_convert].astype(int)
+
+
+# USING SODAPY
 
 def fetch_data():
-    data_path = r'./Data/'
-    if not os.path.exists(data_path):
-        os.makedirs(data_path)
+    ################################
+    # FETCHING AND PROCESSING DATA #
+    ################################
 
-    # Fetching new data
-    URL = 'https://data.cityofnewyork.us/api/views/h9gi-nx95/rows.csv?accessType=DOWNLOAD' # URL to fetch data in csv format
-    r = requests.get(URL, allow_redirects=True)
-    open(os.path.join(data_path, 'collisions.csv'), 'wb').write(r.content) # Downloading csv
+    client = Socrata("data.cityofnewyork.us", APP_TOKEN)
 
-    collisions_csv = csv.read_csv(os.path.join(data_path, 'collisions.csv')) # Opening downloaded csv with pyarrow
-    parquet.write_table(collisions_csv, os.path.join(data_path, 'collisions.parquet')) # Saving csv as parquet file
-    collisions_pq = pq.read_table(os.path.join(data_path, 'collisions.parquet'))
-    if (os.path.join(data_path, 'collisions.parquet')):
-        print('Data successfully downloaded and processed!')
-    else:
-        print('Error in processing data!')
-    os.remove(os.path.join(data_path, 'collisions.csv'))
-    collisions_df = collisions_pq.to_pandas()
-    
+    # Example authenticated client (needed for non-public datasets):
+    # client = Socrata(data.cityofnewyork.us,
+    #                  MyAppToken,
+    #                  userame="user@example.com",
+    #                  password="AFakePassword")
+
+    # First 100 for testing results, returned as JSON from API / converted to Python list of
+    # Change to get_all to fetch entire dataset
+    # dictionaries by sodapy.
+    # results = client.get_all("h9gi-nx95")
+    results = client.get("h9gi-nx95", offset=1400000,limit=2000000, order="crash_date")
+    # Convert to pandas DataFrame
+    results_df = pd.DataFrame.from_records(results)
+
+
     # Cleaning dataset: replacing values identified as erroneous; update as more errors are identified
-    collisions_df.replace({
+    results_df.replace({
                             'unknown': np.nan,
                             'Unknown': np.nan,
                             '': np.nan, 
@@ -39,18 +98,35 @@ def fetch_data():
                             'Illnes': 'Illness',
                             'Drugs (illegal)': 'Drugs (Illegal)'
                             }, inplace=True)
-    collisions_df.drop(['LOCATION', 'ON STREET NAME', 'CROSS STREET NAME', 'OFF STREET NAME', 'VEHICLE TYPE CODE 1', 'VEHICLE TYPE CODE 2', 'VEHICLE TYPE CODE 3', 'VEHICLE TYPE CODE 4', 'VEHICLE TYPE CODE 5', 'CONTRIBUTING FACTOR VEHICLE 2', 'CONTRIBUTING FACTOR VEHICLE 3', 'CONTRIBUTING FACTOR VEHICLE 4', 'CONTRIBUTING FACTOR VEHICLE 5'], axis=1, inplace=True)
+    results_df.drop(['location', 'on_street_name', 'cross_street_name', 'off_street_name', 'vehicle_type_code1', 'vehicle_type_code2', 'vehicle_type_code_3', 'vehicle_type_code_4', 'vehicle_type_code_5', 'contributing_factor_vehicle_2', 'contributing_factor_vehicle_3', 'contributing_factor_vehicle_4', 'contributing_factor_vehicle_5'], axis=1, inplace=True)
     # collisions_df.dropna(inplace=True) # Dropping all rows with missing values; may revist this later to augment data
 
     # Creating new columns for analysis
-    collisions_df['CRASH DATE'] = pd.to_datetime(collisions_df['CRASH DATE'])
-    collisions_df['CRASH YEAR'] = pd.DatetimeIndex(collisions_df['CRASH DATE']).year
-    collisions_df['CRASH MONTH-YEAR'] = pd.to_datetime(collisions_df['CRASH DATE']).dt.to_period('M')
-    collisions_df['CRASH TIME'] = pd.to_datetime(collisions_df['CRASH TIME'])
-    collisions_df['CRASH HOUR'] = pd.DatetimeIndex(collisions_df['CRASH TIME']).hour
+    results_df['crash_date'] = pd.to_datetime(results_df['crash_date'])
+    results_df['crash_year'] = pd.DatetimeIndex(results_df['crash_date']).year
+    results_df['crash_month_year'] = pd.to_datetime(results_df['crash_date']).dt.to_period('M')
+    results_df['crash_time'] = pd.to_datetime(results_df['crash_time'])
+    results_df['crash_hour'] = pd.DatetimeIndex(results_df['crash_time']).hour
+
+    results_df.number_of_persons_injured.fillna(0, inplace=True)
+    results_df.number_of_persons_killed.fillna(0, inplace=True)
+    results_df.number_of_pedestrians_injured.fillna(0, inplace=True)
+    results_df.number_of_pedestrians_killed.fillna(0, inplace=True)
+    results_df.number_of_cyclist_injured.fillna(0, inplace=True)
+    results_df.number_of_cyclist_killed.fillna(0, inplace=True)
+    results_df.number_of_motorist_injured.fillna(0, inplace=True)
+    results_df.number_of_motorist_killed.fillna(0, inplace=True)
+
+    # Converting objects to int
+    cols_to_convert = ['number_of_persons_injured', 'number_of_persons_killed',
+        'number_of_pedestrians_injured', 'number_of_pedestrians_killed',
+        'number_of_cyclist_injured', 'number_of_cyclist_killed',
+        'number_of_motorist_injured', 'number_of_motorist_killed']
+    results_df[cols_to_convert] = results_df[cols_to_convert].astype(int)
+    client.close()
 
 
-    return collisions_df    
+    return results_df    
 
 def main():
     fetch_data()
