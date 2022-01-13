7b1cbb54bf87a28f19a8228f18162fbb6a7d549f
Roger Lefort <tirogerl@gmail.com>
2022-01-12 23:52:22 -0800

    Added new visualizations

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 # LIBRARIES #
 #############
 
+from os import rename
+from streamlit.state.session_state import Value
 from get_data import fetch_data # Module to fetch and process data
 import pandas as pd
 import numpy as np
@@ -12,6 +14,14 @@ from folium.plugins import HeatMap
 from streamlit_folium import folium_static
 import plotly
 import datetime
+import plotly.express as px
+import plotly.graph_objects as go
+
+import pyarrow.parquet as pq
+import awswrangler as wr
+import calendar
+
+data_URI = 's3://nypdcollisions/collisions.parquet'
 
 from PIL import Image
 
@@ -39,7 +49,7 @@ st.set_page_config(
 # FETCHING DATA #
 # ###############
 
-@st.cache(show_spinner=False)
+@st.cache(show_spinner=False, max_entries=5, ttl=86400)
 def fetch_and_clean_data():
     with st.spinner('Data Refreshing... May take up to 5 minutes.'):
         collisions_df = fetch_data()
@@ -126,9 +136,35 @@ peds_killed_perc_change = (peds_killed_YTD.values - peds_killed_YTD_previous.val
 cyclists_injured_perc_change = (cyclists_injured_YTD.values - cyclists_injured_YTD_previous.values)/cyclists_injured_YTD_previous.values*100
 cyclists_killed_perc_change = (cyclists_killed_YTD.values - cyclists_killed_YTD_previous.values)/cyclists_killed_YTD_previous.values*100
 
-####################
-# DATA FOR MAPPING #
-####################
+############################
+# FETCHING HISTORICAL DATA #
+############################
+
+# Retrieving the data directly from Amazon S3
+all_collisions_df = wr.s3.read_parquet(data_URI)
+# Modifying column names
+all_collisions_df.columns = all_collisions_df.columns.str.lower()
+all_collisions_df.columns = all_collisions_df.columns.str.replace(' ', '_')
+
+all_collisions_df['crash_date'] = pd.to_datetime(all_collisions_df['crash_date'])
+all_collisions_df['crash_year'] = pd.DatetimeIndex(all_collisions_df['crash_date']).year
+all_collisions_df['crash_month'] = pd.DatetimeIndex(all_collisions_df['crash_date']).month
+# all_collisions_df['crash_month'] = all_collisions_df['crash_date'].dt.month_name()
+all_collisions_df['crash_month_year'] = pd.to_datetime(all_collisions_df['crash_date']).dt.to_period('M')
+historical_df = all_collisions_df[all_collisions_df.crash_date < '2022-01-01'] # Removing 2022 data
+historical_df = all_collisions_df[all_collisions_df.crash_year > 2012]
+
+by_year_and_boro = historical_df.groupby(['crash_year', 'crash_month']).agg({
+    'collision_id': 'count',
+    'number_of_persons_injured': 'sum',
+    'number_of_persons_killed': 'sum',
+    'number_of_pedestrians_injured': 'sum',
+    'number_of_pedestrians_killed': 'sum',
+    'number_of_cyclist_injured': 'sum',
+    'number_of_cyclist_killed': 'sum',
+    'number_of_motorist_injured': 'sum',
+    'number_of_motorist_killed': 'sum'
+}).rename(columns={'collision_id': 'total_collisions'}).reset_index()
 
 ##########################
 # COLLISIONS IN PAST DAY #
@@ -253,11 +289,115 @@ with st.container():
     st.markdown(f'<h5>There were <em style="font-size:30px" "color:red">{total_collisions}</em> collisions on {latest_collision_date}</h5>', unsafe_allow_html=True)
     map_collisions(latest_collision_date)
     
+st.markdown('<hr/>', unsafe_allow_html=True)
 
+###################
+# HISTORICAL DATA #
+###################
+
+st.markdown(f'### Historical Data from 2013 to 2021')
+col8, col9 = st.columns(2)
+
+metrics = ['total_collisions', 'number_of_persons_injured', 'number_of_persons_killed', 'number_of_pedestrians_injured', 'number_of_pedestrians_killed', 'number_of_cyclist_injured', 'number_of_cyclist_killed', 'number_of_motorist_injured', 'number_of_motorist_killed']
+month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+
+def change_case(string):
+    no_under = string.replace('_', ' ')
+    final = no_under.title()
+    return final
+
+with col8:
+    metric = st.selectbox('', options=metrics, format_func=lambda x: change_case(x), help='Select desired metric')
+    proper_metric = metric.replace('_', ' ')
+    proper_metric = proper_metric.title()
+
+    fig = px.line(by_year_and_boro, x="crash_month", y=metric, color='crash_year', markers=True, title=f'{proper_metric} between 2012 and 2021 <br><sup>Data may not be complete due to missing borough labels.</sup>' , labels={
+                        'crash_year': 'Year',
+                        "crash_month": 'Month',
+                        metric: proper_metric,
+                    })
+
+    # fig.for_each_trace(lambda trace: fig.add_annotation(
+    #     x=trace.x[-1], y=trace.y[-1], text='  '+trace.name, 
+    #     font_color=trace.line.color,
+    #     ax=10, ay=10, xanchor="left", showarrow=False))
+
+    fig.update_xaxes(ticktext=month_names, tickvals=[1,2,3,4,5,6,7,8,9,10,11,12])    
+    fig.update_layout(
+            xaxis=dict(
+                showline=True,
+                showgrid=False,
+                showticklabels=True,
+                linecolor='rgb(204, 204, 204)',
+                linewidth=2,
+                ticks='outside',
+                title='',
+                tickfont=dict(
+                    family='Arial',
+                    size=12,
+                    color='rgb(82, 82, 82)',
+                ),
+            ),
+            yaxis=dict(
+                # showgrid=True,
+                zeroline=False,
+                showline=True,
+                gridcolor = 'rgb(235, 236, 240)',
+                showticklabels=True,
+                title='',
+                autorange=True
+            ),
+            autosize=True,
+            hovermode="x unified",
+            margin=dict(
+                autoexpand=True,
+                l=100,
+                r=20,
+                t=110,
+            ),
+            showlegend=True,
+    #         legend=dict(
+    #         # orientation="h",
+    #         yanchor="bottom",
+    #         y=0.9,
+    #         xanchor="left",
+    #         x=0.7
+    # ),
+            plot_bgcolor='rgba(0,0,0,0)'
+        )
+    # fig.show()
+
+    st.plotly_chart(fig, use_container_width = True)
+
+with col9:
+    n_reasons = st.slider('Slide to see more', min_value=5, max_value=20, value=5)
+    crash_causes = historical_df['contributing_factor_vehicle_1'].value_counts().rename_axis('unique_values').reset_index(name='counts')
+    top_10_crashes_causes = crash_causes[crash_causes.unique_values != 'Unspecified'].head(n_reasons)
+
+    labels = top_10_crashes_causes.unique_values
+    values = top_10_crashes_causes.counts
+
+# pull is given as a fraction of the pie radius
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, pull=[0.2,0,0,0,0,0,0,0,0,0,0])])
+    fig.update_layout(
+    title_text=f'Top {n_reasons} Vehicle Collision Causes <br><sup>~30% of causes listed as "Unspecified" .</sup>')
+
+#     fig = px.bar(top_10_crashes_causes, x="unique_values", y="counts")
+    st.plotly_chart(fig, use_container_width = True)
+
+
+
+
+############
+# SIDE BAR #
+############
 
 st.sidebar.subheader("About")
+st.sidebar.markdown('*In 2011 the New York City Council passed Local Law #12 requiring the NYPD to collect and make available to the public records of all accidents involving motor vehicles. A police report (MV104-AN) is required to be filled out for collisions where someone is injured or killed, or where there is at least $1000 worth of damage. Each report represents a unique collision and includes date and time, location(city, borough, address, zip code, latitude and longitude), number of injuries and/or deaths, contributing factors, and information about the vehicles involved. The information is available in a variety of formats, including downloadable json and csv files, and through the Socrata Open Data API.*')
 
-st.sidebar.caption(f'Data between {first_date.strftime("%m/%d/%Y")} and {last_updated.strftime("%m/%d/%Y")}')
+st.sidebar.caption(f'Data current as of {last_updated.strftime("%m/%d/%Y")}')
+st.sidebar.markdown('<hr/>', unsafe_allow_html=True)
 st.sidebar.text('Dashboard created by Roger Lefort')
 
 
